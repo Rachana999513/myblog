4d1b526a3cdf669254fcd1fe40ad1c104d1622c8
Rachana999513 <82526585+Rachana999513@users.noreply.github.com>
2021-07-09 19:42:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,68 @@
-function submit()
+ last_position_of_x, last_position_of_y;
+color = "black";
+width_of_line = 2;
+
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+var width = screen.width;
+
+new_width = screen.width - 70;
+new_heigth = screen.height - 300;
+if(width < 992)
 {
-    var display_student_array = [];
+    document.getElementById("myCanvas").width = new_width;
+    document.getElementById("myCanvas").height = new_height;
+    document.body.style.overflow = "hidden";
+}
+canvas.addEventListener("touchstart", my_touchstart);
 
-    for (var j = 1; j <= 4; j++)
+function my_touchstart(e)
+{
+    console.log("my_touchstart", my_touchstart);
 
+    function my_touchstart(e)
     {
-        var name_of_the_student = document.getElementById("name_of_the_student_"+j ) .value;
-        console.log(name_of_the_student) ;
-        name_of_the_student_array.push(name_of_the_student_student) ;
+        console.log("my_touchstart");
+        //Additional Activity
+        color = document.getElementById("color").Value;
+        width_of_line = document.getElementById("width_of_line").value;
+        //End Additional Activity
+
+        last_position_x = e.touches[e].clientX - canvas.offsetLeft;
+        last_position_y = e.touches[0].clientY - canva.offsetTop;
     }
 
+    canvas.addEventListener("touchmove", my_touchmove);
 
-    console.log(name_of_the_student_array);
-    var length_of_name_of_students_array = name_of_the_student_array.length;
-    console.log (length_of_name_of_students_array);
-
-    for (var k = 0; k < length_of_name_of_students_array; k++)
-
+    function my_touchmove(e)
     {
-        display_student_array.push("<h4>NAME - "+ name_of_the_student_array[k] + "</h4>");
-        console.log(display_student_array);
+        console.log("my_touchMove"); 
+        current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
+         current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
+          // old same old as the paint web app ctx.beginPath();
+           ctx.strokeStyle = color; ctx.lineWidth = width_of_line;
 
+            console.log("Last position of x and y coordinates = "); 
+
+            console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
+
+             ctx.moveTo(last_position_of_x, last_position_of_y); 
+
+             console.log("Current position of x and y coordinates = "); 
+
+             console.log("x = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
+
+              ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y); ctx.stroke();
+
+               last_position_of_x = current_position_of_touch_x; last_position_of_y = current_position_of_touch_y;
+
+// end of old same old as the paint web app 
     }
 
-    console.log(display_student_array);
-    document.getElementById("display_name_with_commas").innerHTML = display_student_array;
-
-    var remove_commas = display_student_array.join("");
-    console.log(remove_commas);
-    document.getElementById("dispay_name_without_commas").innerHTML = remove_commas ;
-
-
-    document.getElementById("submit_button").style.display = "none";
-    document.getElementById("sort_button").style.display = "inline-block";;
-
+    // old same old as the paint web app
+    function cleararea() {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    }
+    // end of old same old as the paint web app 
 }
