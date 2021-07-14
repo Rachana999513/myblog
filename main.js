48d2cbb76914ed9a0e1033bf47823712b70daca1
Rachana999513 <82526585+Rachana999513@users.noreply.github.com>
2021-07-14 19:45:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
 
+nasa_mars_images_array = ["nasa_image_1.jpg","nasa_image_2.jpeg", "nasa_image_3.jpg","nasa_image_4.jpg"];
+
+random_number = Math.floor(Math.random()* 4); 
+console.log(random_number);
 rover_width = 100;
 rover_height = 90;
 
-background_image = "mars.jpg";
 
+background_image =  nasa_mars_images_array[random_number ];
+console.log("background_image = " + background_image);
 rover_image = "rover.png";
 
 rover_x = 10;
