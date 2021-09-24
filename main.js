5bffc534223e7ced2266b5ee0e66009584b542bc
Rachana999513 <82526585+Rachana999513@users.noreply.github.com>
2021-09-24 19:38:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,57 +1,25 @@
-var SpeechRecognition = window.webkitSpeechRecognition;
-
-var recognition = new SpeechRecognition();
-
-function start()
- { document.getElementById("textbox").innerHTML = ""; 
- recognition.start(); } 
- recognition.onresult = function(event)
-  {
-       console.log(event); 
-    var Content = event.results[0][0].transcript; 
-    document.getElementById("textbox").innerHTML = Content;
-     console.log(Content); if(Content =="take my selfie") { console.log("taking selfie --- "); 
-     speak(); }
-    
-    }
-
-function speak(){
-    var synth = window.speechSynthesis;
-
-    speak_data = "Taking you Selfie in 5 seconds";
-
-    var utterThis = new SpeechSynthesisUtterance(speak_data);
-
-    synth.speak(utterThis);
-    Webcam.attach(camera);
-
-    setTimeout(function()
-    {
-        take_snapshot();
-        save();
-    }, 5000);
-}
-camera = document.getElementById("camera");
-Webcam.set({
-    width:360,
-    height:250,
-    image_format : 'jpeg',
-    png_quality:90
-});
-
-
-function take_snapshot()
-{
-    Webcam.snap(function(data_url) {
-        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_url+'"/>';
-
-    });
+function preload() {
 }
 
-function save()
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    tint_color = "";
+}
+
+function draw() {
+    image(video, 0, 0, 640, 480);
+    tint(tint_color);
+}
+
+function take_snapshot(){
+    save('Rachana.png');
+}
+
+function filter_tint()
 {
-    link = document.getElementById("link");
-    image = document.getElementById("selfie_image").src ;
-    link.href = image;
-    link.click();
+    tint_color = document.getElementById("color_name").value;
 }
