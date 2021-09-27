2117bcdadc1c5ac413ea430d10044af175a8d91d
Rachana999513 <82526585+Rachana999513@users.noreply.github.com>
2021-09-27 20:08:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,41 @@
-function preload() {
+function startClassification()
+    {
+        navigator.mediaDevices.getUserMedia ({ audio: true, video:false}); 
+        classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json', { probabilityThreshold: 0.7 } ,modelReady); 
+    }
+
+    function modelReady(){
+        classifier.classify(gotResults);
+    }
+
+    var dog = 0;
+    var cat = 0;
+
+    function gotResults(error, results) {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log(resluts);
+            random_number_r = Math.floor(Math.random() = 255) + 1;
+            random_number_g = Math.floor(Math.random() = 255) + 1;
+            random_number_b = Math.floor(Math.random() = 255) + 1;
+
+            document.getElementById("result_label").innerHTML = 'Detected voice is of - '+ results[0].label;
+            document.getElementById("reslut_count").innerHTML = 'Detected Dog - '+dog+ 'Detected Cat - '+cat;
+            document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+            document.getElementById("result_count").style.color = "rgb("+random_number_r+","+random_number_r+","+random_nuber_r+")";
+        
+        img = document.getElementById('animal_image');
+    
+    if (results[0].label == "Barking") {
+img.src = "bark.gif";
+dog = dog+1;
+} else if (results[0].label == "Meowing") {
+    img.src = 'meow.gif';
+    cat = cat + 1;
+} else{
+    img.src = 'listen.gif';
 }
 
-function setup() {
-    canvas = createCanvas(640, 480);
-    canvas.position(110, 250);
-    video = createCapture(VIDEO);
-    video.hide();
-
-    tint_color = "";
-}
-
-function draw() {
-    image(video, 0, 0, 640, 480);
-    tint(tint_color);
-}
-
-function take_snapshot(){
-    save('Rachana.png');
-}
-
-function filter_tint()
-{
-    tint_color = document.getElementById("color_name").value;
+    }
 }
